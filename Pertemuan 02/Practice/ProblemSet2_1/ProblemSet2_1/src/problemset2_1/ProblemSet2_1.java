01a6c451ce805c32772b59a2ee48c6da16b52cff
Steffany21 <97678944+Steffany21@users.noreply.github.com>
2022-03-20 16:05:15 +0700

    Practice

---
+++ Pertemuan 02/Practice/ProblemSet2_1/ProblemSet2_1/src/problemset2_1/ProblemSet2_1.java
@@ -0,0 +1,19 @@
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void draw(){
+        System.out.println("  ----------  ");
+        System.out.println("--          --");
+}
+    public static void main(String[] args) {
+    draw();
+        System.out.println("|            |");
+        System.out.println("|   /\\   /\\  |");
+        System.out.println("|      >     |");
+        System.out.println("|   \\____/   |");
+        System.out.println("|            |");
+        System.out.println("--          --");
+        System.out.println("  -----------  ");
+              
+    }   
+}
