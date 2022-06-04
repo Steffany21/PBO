1202a28e5773636d65fc3b7feee5d96a3cfcf2b4
Steffany21 <97678944+Steffany21@users.noreply.github.com>
2022-06-04 22:14:44 +0700

    Pertemuan 09

---
+++ Pertemuan 09/GoalTest_Student/GoalTest_Student/src/goaltest/Goal.java
@@ -0,0 +1,16 @@
+package goaltest;
+
+public class Goal {
+
+    public Goal(){
+
+        
+        GoalTest.root.getChildren().addAll();
+        interactions();
+    }
+    
+    private void interactions(){
+        //Exercise 4   
+        
+    }
+}
